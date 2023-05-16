3d58acff3f55d238da1a4b4aa18a64f7f37b82bc
Masato282828 <masato7010@icloud.com>
2023-05-16 13:37:12 +0900

    test

---
+++ resources/views/shops/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Flug</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>古着屋巡り用のブログ</h1>
+        <div class='shops'>
+            @foreach ($shops as $shop)
+                <div class='shop'>
+                    <a href="/shops/{{ $shop->id }}"><h2 class='name'>{{ $shop->name }}</h2></a>
+                    <p class='overview'>{{ $shop->overview }}</p>
+                </div>
+            @endforeach    
+        </div>
+    </body>
+</html>
